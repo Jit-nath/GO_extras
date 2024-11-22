d4bfd8e722185de1919bee3d53a5c2f5763965ee
Jit Debnath <jit.nathdeb@gmail.com>
2024-11-22 14:58:41 +0530

    x

--- server.go
+++ server.go
@@ -7,14 +7,14 @@ import (
 
 func startServer() {
 
-	var port string = ":8080"
-	var address string = "127.0.0.1"
+	port := ":80"
+	address := "127.0.0.1"
 
 	fs := http.FileServer(http.Dir("./dist"))
 	http.Handle("/", fs)
 	log.Printf("Served at http://%s", address+port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
-		log.Fatalf("Error starting the server: %v", err)
+		log.Fatal(err)
 	}
 }
